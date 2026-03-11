be8eea451c00a7e14ed57ad9f9f782cd8f920783
OmitHasanAdor <ibneshams05@gmail.com>
2026-03-12 02:22:00 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,15 @@
+document.getElementById('loginBtn').addEventListener('click',()=>{
+const inputUsername =document.getElementById('userName')
+const usernameValue=inputUsername.value 
+
+const inputPass=document.getElementById('inputPass')
+const passValue=inputPass.value 
+
+if (usernameValue=='admin' && passValue=='admin123') {
+    window.location.assign('/home.html')
+} else {
+    alert('login failed')
+}
+
+
+})
